b7542ca98a8a0fb13fd0504ea4aceeedb1563b7b
tangxiaolv <tangxiaolv@syswin.com>
2017-05-19 17:50:35 +0800

    push lib

--- surgeon-core/src/main/java/com/surgeon/weaving/core/Surgeon.java
+++ surgeon-core/src/main/java/com/surgeon/weaving/core/Surgeon.java
@@ -1,11 +1,4 @@
 package com.surgeon.weaving.core;
 
-/**
- * <li>Description : <li>Created by tangxiaolv on 2017/5/19. <li>Job number：138710 <li>Phone
- * ：13601100793 <li>Email：tangxiaolv@syswin.com <li>Person in charge : tangxiaolv
- * <li>Leader：tangxiaolv
- */
-
-
 public class Surgeon {
 }
